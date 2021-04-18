610a0f25c1680fa58b64ccf8f1f20bbf510fe6c4
cmr1357 <79671395+cmr1357@users.noreply.github.com>
2021-04-18 06:26:03 -0600

    Laser_Emmit

---
+++ Laser_Emmit.ino
@@ -0,0 +1,17 @@
+
+/////////////////////////////////////////LASER EMIT MODULE///////////////////////////////////////
+
+int laserPin = 7;
+
+void setup ()
+{
+  pinMode(laserPin, OUTPUT);
+}
+
+void loop () 
+{
+  digitalWrite(laserPin, HIGH);
+  delay(100);
+  digitalWrite(laserPin, LOW);
+  delay(100);
+}
